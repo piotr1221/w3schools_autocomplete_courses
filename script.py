3dc9db4bab8b45c8a7bc0308def8d7af11fe7012
Edwin Aguilar <edwin.aguilar@unmsm.edu.pe>
2022-03-03 17:50:52 -0500

    complete tutorial

--- script.py
+++ script.py
@@ -20,17 +20,27 @@ def login(driver, args):
 def select_course(driver, args, course_choices):
     explore_all = driver.find_element(By.XPATH, os.getenv('explore_all'))
     driver.execute_script("arguments[0].click();", explore_all)
-    all_courses = driver.find_element(By.XPATH, os.getenv('all_courses_list'))
     
     for i in range(1, len(course_choices) + 1):
-        course = all_courses.find_element(By.XPATH, f'./div[{i}]{os.getenv("course_title")}')
-        if args.COURSE in course.text:
-            course.click()
-            break
-    
+        all_courses = driver.find_element(By.XPATH, os.getenv('all_courses_list'))
+        possible_course_titles = all_courses.find_elements(By.XPATH, f'./div[{i}]{os.getenv("course_title_parent")}/*')
+        for course_title in possible_course_titles:
+            if args.COURSE in course_title.text:
+                course_title.click()
+                return
+        
 def complete_tutorial(driver, args):
     driver.find_element(By.XPATH, f'//*[contains(text(), \"{os.getenv("tutorial_tab")}\")]').click()
+    nav = driver.find_element(By.ID, os.getenv('tutorial_nav'))
+    home = os.getenv("home")
+    nav.find_element(By.XPATH, f'./a[contains(translate(., "{home.lower()}", "{home.upper()}"), "{home.upper()}")]').click()
     
+    while driver.find_elements(By.XPATH, f'//a[contains(text(), \"{os.getenv("next")}\")]'):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
+        #driver.execute_script("window.scrollTo(document.body.scrollHeight/2, document.body.scrollHeight);")
+        driver.find_element(By.XPATH, f'//a[contains(text(), \"{os.getenv("next")}\")]').click()
+
 def main(args, course_choices):
     load_dotenv()
     with Firefox(options=get_options()) as driver:
